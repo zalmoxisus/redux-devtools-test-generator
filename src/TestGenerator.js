5596425a256e7b729e2f971a91dd799cf8227ad9
Zalmoxisus <zalmoxisus@gmail.com>
2017-06-03 14:17:35 +0300

    Support React 15.5.0

--- src/TestGenerator.js
+++ src/TestGenerator.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, Component, PropTypes } from 'react';
+import React, { PureComponent, Component } from 'react';
+import PropTypes from 'prop-types';
 import stringify from 'javascript-stringify';
 import objectPath from 'object-path';
 import jsan from 'jsan';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Toolbar, Container, Button, Select, Notification, Dialog } from 'devui';
 import { formSchema, uiSchema, defaultFormData } from './templateForm';
 import AddIcon from 'react-icons/lib/md/add';
